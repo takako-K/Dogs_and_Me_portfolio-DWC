443c8a06f3414701dd6ca42df92bde576e5d97bd
takako-K <takako.k828@gmail.com>
2021-01-20 11:42:39 +0000

    [Add]Usersテーブルにintroductionカラム・profile_image_idカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_112113) do
+ActiveRecord::Schema.define(version: 2021_01_20_114019) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2021_01_20_112113) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduction"
+    t.text "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
