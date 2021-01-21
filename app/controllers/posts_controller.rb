8be57fc0453b632d47a05b47965f037ee77025dc
takako-K <takako.k828@gmail.com>
2021-01-21 07:07:02 +0000

    [Add]deviseのセットアップ、users.postsコントローラーの作成、ルートの設定

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/about', to: 'homes#about'
+  resources :users
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
