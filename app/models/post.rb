0211641d15ddbdab70b39cdf566a44245861bd8d
takako-K <takako.k828@gmail.com>
2021-01-23 13:31:27 +0000

    [Make]post_commentモデルアソシエーション作成、コントローラー作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
   belongs_to :user, optional: true
+  has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 100 }
