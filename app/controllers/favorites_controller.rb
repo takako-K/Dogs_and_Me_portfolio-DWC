d2cc0f787610e2774d372c8bc5f724371cf8e3eb
takako-K <takako.k828@gmail.com>
2021-01-29 05:35:45 +0000

    [Add]いいね機能非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_to request.referer     # 元の画面に遷移する
+    #redirect_to request.referer     # 元の画面に遷移する
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to request.referer     # 元の画面に遷移する
+    #redirect_to request.referer     # 元の画面に遷移する
   end
 end
