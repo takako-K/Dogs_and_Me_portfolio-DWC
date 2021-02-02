7c62d80cd05232887aae17df4fdea4896f904341
takako-K <takako.k828@gmail.com>
2021-02-02 13:39:49 +0000

    [Fix]fullcalendar一部エラー修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,11 +7,13 @@ class EventsController < ApplicationController
     @event = Event.new
     @user = current_user
     @events = Event.where(user_id: current_user.id)
-    @today_events = @events.where("start <= ? AND end >= ?", Date.today.end_of_day, Date.today.beginning_of_day)       # eventモデル利用して本日の予定表示
+    # eventモデル利用して本日の予定表示
+    @today_events = @events.where("start <= ? AND end >= ?", Date.today.end_of_day, Date.today.beginning_of_day)
   end
 
   def create
     @user = current_user
+    @events = Event.where(user_id: current_user.id)
     event = Event.new(event_params)
     # 終日を選択した場合、自動的にstart,endカラムに値が入る
     if event != nil && event.allday == true
@@ -19,15 +21,25 @@ class EventsController < ApplicationController
       event.end = event.end.strftime('%Y/%m/%d')
     end
     event.save!
-    flash.now[:success] = "予定を登録しました"            # リダイレクトさせればメッセージ出力？
-    @events = Event.where(user_id: current_user.id)
+    flash[:success] = "予定を登録しました"
+    redirect_to request.referer
   end
 
   def update
     event = Event.find(params[:id])
     @events = Event.where(user_id: current_user.id)
-    event.update(event_params)
-    flash.now[:success] = "予定を更新しました"
+    # 終日を選択した場合、自動的にstart,endカラムに値が入る
+    if params[:event] != nil && params[:event][:allday] == true
+      allday_event_start = params[:event][:start].strftme('%Y/%m/%d')
+      allday_event_end = params[:event][:end].strftime('%Y/%m/%d')
+      event.update(event_params)
+      event.start = allday_event_start
+      event.end = allday_event_end
+    else
+      event.update(event_params)
+    end
+    flash[:success] = "予定を更新しました"
+    redirect_to request.referer
   end
 
   def destroy
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates :start, presence: true
+  validates :end, presence: true
 end
