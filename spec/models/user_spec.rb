123684e890c076021c6c3dcbbbad3df4435ad8a5
takako-K <takako.k828@gmail.com>
2021-02-04 06:36:37 +0000

    [Add]Userモデル、テストコード記述終了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,77 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-Rspec.describe 'Userモデルのテスト', type: :model do
+RSpec.describe 'Userモデルのテスト', type: :model do
   describe 'アソシエーションのテスト' do
     context 'Postモデルとの関係' do
       it '1:Nとなっている' do
-        except(User.reflect_on_association(:posts).macro).to eq :has_many
+        expect(User.reflect_on_association(:posts).macro).to eq :has_many
       end
     end
     context 'Favoriteモデルとの関係' do
       it '1:Nとなっている' do
-        except(User.reflect_on_association(:favorites).macro).to eq :has_many
+        expect(User.reflect_on_association(:favorites).macro).to eq :has_many
       end
     end
     context 'PostCommentモデルとの関係' do
       it '1:Nとなっている' do
-        except(User.reflect_on_association(:post_comments).macro).to eq :has_many
+        expect(User.reflect_on_association(:post_comments).macro).to eq :has_many
       end
     end
     context 'Eventモデルとの関係' do
       it '1:Nとなっている' do
-        except(User.reflect_on_association(:events).macro).to eq :has_many
+        expect(User.reflect_on_association(:events).macro).to eq :has_many
       end
     end
     context 'Notificationモデルとの関係' do
       it '1:Nとなっている' do
-        except(User.reflect_on_association(:notifications).macro).to eq :has_many
+        expect(User.reflect_on_association(:notifications).macro).to eq :has_many
       end
     end
   end
-  
+
   describe 'バリデーションのテスト' do
     subject { user.valid? }
-    
+
     let!(:other_user) { create(:user) }
     let(:user) { build(:user) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        user.name = ''
+        is_expected.to eq false
+      end
+      it '2文字以上であること：1文字はNG' do
+        user.name = Faker::Lorem.characters(number: 1)
+        is_expected.to eq false
+      end
+      it '2文字以上であること：2文字はOK' do
+        user.name = Faker::Lorem.characters(number: 2)
+        is_expected.to eq true
+      end
+      it '20文字以下であること：20文字はOK' do
+        user.name = Faker::Lorem.characters(number: 20)
+        is_expected.to eq true
+      end
+      it '20文字以下であること：21文字はNG' do
+        user.name = Faker::Lorem.characters(number: 21)
+        is_expected.to eq false
+      end
+      it '一意性があること' do
+        user.name = other_user.name
+        is_expected.to eq false
+      end
+    end
+
+    context 'introductionカラム' do
+      it '100文字以下であること：100文字はOK' do
+        user.introduction = Faker::Lorem.characters(number: 100)
+        is_expected.to eq true
+      end
+      it '100文字以下であること：101文字はNG' do
+        user.introduction = Faker::Lorem.characters(number: 101)
+        is_expected.to eq false
+      end
+    end
   end
 end
