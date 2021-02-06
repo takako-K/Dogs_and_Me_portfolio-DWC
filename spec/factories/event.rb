868cfa6a857973d815f5ce78781ffcbc56a2b469
takako-K <takako.k828@gmail.com>
2021-02-06 13:51:55 +0000

    [Fix]RSpecテストコード修正

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :event do
     title { Faker::Lorem.characters(number: 10) }
     body { Faker::Lorem.characters(number: 50) }
+    allday { true }
     association :user, factory: :user
-    # allday { true }
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'Notificationモデルのテスト', type: :model do
   describe 'アソシエーションのテスト' do
     context 'Userモデルとの関係' do
       it 'N:1となっている' do
-        #expect(Notification.reflect_on_association(:user).macro).to eq :belongs_to
         expect(Notification.reflect_on_association(:visiter).macro).to eq :belongs_to
         expect(Notification.reflect_on_association(:visited).macro).to eq :belongs_to
       end
