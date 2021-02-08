c4f0d7458bb9d3e5ea23d121e40ad4730923a51a
takako-K <takako.k828@gmail.com>
2021-02-08 06:38:23 +0000

    [Fix]Rubocopコード修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,15 +2,15 @@ module NotificationsHelper
   def notification_form(notification)
     @visiter = notification.visiter
     @post_comment = nil
-    your_post = link_to 'あなたの投稿', post_path(notification.post_id), style: "font-weight: bold;"
+    @your_post = link_to 'あなたの投稿', post_path(notification.post_id), style: "font-weight: bold;"
     @visiter_post_comment = notification.post_comment_id
     # notification.actionが、favoriteかpost_commentか
     case notification.action
-    when "favorite" then
-      tag.a(notification.visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a('あなたの投稿', href: post_path(notification.post_id), style: "font-weight: bold;") + "にいいねしました。"
-    when "post_comment" then
+    when "favorite"
+      tag.a(notification.visiter.name, href: user_path(@visiter), style: 'font-weight: bold;') + "が" + tag.a("あなたの投稿", href: post_path(notification.post_id), style: 'font-weight: bold;') + "にいいねしました。"
+    when "post_comment"
       @post_comment = PostComment.find_by(id: @visiter_post_comment)&.comment
-      tag.a(@visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a('あなたの投稿', href: post_path(notification.post_id), style: "font-weight: bold;") + "にコメントしました。"
+      tag.a(@visiter.name, href: user_path(@visiter), style: 'font-weight: bold;') + "が" + tag.a("あなたの投稿", href: post_path(notification.post_id), style: 'font-weight: bold;') + "にコメントしました。"
     end
   end
 
