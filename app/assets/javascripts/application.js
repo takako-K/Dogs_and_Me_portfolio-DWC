dbb66d023a07a82ce74411868f1c6e143a2508c2
takako-K <takako.k828@gmail.com>
2021-02-08 13:01:38 +0000

    [Fix]トップ画面背景修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,13 +23,13 @@
 // top画面bg-slider
 $(window).load(function() {
   $('.bg-slider').bgSwitcher({
-    images: ['assets/angel-luciano-LATYeZyw88c-unsplash.jpg', 'assets/pauline-loroy-U3aF7hgUSrk-unsplash.jpg', 'assets/anna-dudkova-urs_y9NwFcc-unsplash.jpg', 'assets/jamie-street-wcO2PWLuQ3U-unsplash.jpg'], // 切り替える背景画像を指定
-    interval: 7000, // 背景画像を切り替える間隔を指定 3000=3秒
-    loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
-    shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
-    effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
-    duration: 2000, // エフェクトの時間を指定します。
-    easing: "swing" // エフェクトのイージングをlinear,swingから指定
+    images: ['assets/angel-luciano-LATYeZyw88c-unsplash.jpg', 'assets/pauline-loroy-U3aF7hgUSrk-unsplash.jpg', 'assets/anna-dudkova-urs_y9NwFcc-unsplash.jpg', 'assets/jamie-street-wcO2PWLuQ3U-unsplash.jpg'], // 背景画像
+    interval: 7000,          // 背景画像を7秒で切り替える
+    loop: true,              // 切り替えを繰り返す
+    shuffle: true,           // 背景画像の順番をシャッフルする
+    effect: "fade",          // エフェクトの種類(fade,blind,clip,slide,drop,hide)
+    duration: 2000,          // エフェクトの時間を指定
+    easing: "swing"          // エフェクトのイージング（linear,swing）
 
   });
 
